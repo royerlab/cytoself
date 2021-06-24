913df3b28606099d87e0a9157065ab6fccecb6f8
li-li-github <liamiiliil@gmail.com>
2021-06-23 17:01:13 -0700

    setup.py updated.

--- setup.py
+++ setup.py
@@ -1,26 +1,28 @@
-import os
+from os import path
 import codecs
 import setuptools
 
-# # Load long description
-# with open("README.md", "r", encoding="utf-8") as fh:
-#     long_description = fh.read()
+# Load long description
+with open(
+    path.join(path.abspath(path.dirname(__file__)), "README.md"), "r", encoding="utf-8"
+) as fh:
+    long_description = fh.read()
 
-with open('requirements.txt') as f:
+with open(path.join(path.abspath(path.dirname(__file__)), "requirements.txt")) as f:
     default_requirements = [
-        line.strip() for line in f if line and not line.startswith('#')
+        line.strip() for line in f if line and not line.startswith("#")
     ]
 
 
 def read(rel_path):
-    here = os.path.abspath(os.path.dirname(__file__))
-    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+    here = path.abspath(path.dirname(__file__))
+    with codecs.open(path.join(here, rel_path), "r") as fp:
         return fp.read()
 
 
 def get_version(rel_path):
     for line in read(rel_path).splitlines():
-        if line.startswith('__version__'):
+        if line.startswith("__version__"):
             delim = '"' if '"' in line else "'"
             return line.split(delim)[1]
     else:
@@ -28,14 +30,16 @@ def get_version(rel_path):
 
 
 setuptools.setup(
-    name='cytoself',
-    version=get_version("cytoself/__init__.py"),  # cytoself.__version__,  #
-    author='',
-    author_email='',
-    description='An image feature extractor with self-supervised learning',
-    url='https://github.com/royerlab/cytoself',
+    name="cytoself",
+    version=get_version("cytoself/__init__.py"),
+    author="",
+    author_email="",
+    description="An image feature extractor with self-supervised learning",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/royerlab/cytoself",
     project_urls={
-        "Bug Tracker": 'https://github.com/royerlab/cytoself/issues',
+        "Bug Tracker": "https://github.com/royerlab/cytoself/issues",
     },
     classifiers=[
         "Programming Language :: Python :: 3 :: Only",
@@ -50,7 +54,7 @@ setuptools.setup(
         "Topic :: Scientific/Engineering :: Image Recognition",
         "Topic :: Scientific/Engineering :: Information Analysis",
     ],
-    package_dir={"": f".{os.path.sep}"},
+    package_dir={"": f".{path.sep}"},
     packages=setuptools.find_packages(),
     install_requires=default_requirements,
     python_requires=">=3.6, <3.8",
