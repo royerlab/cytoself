a98cd313b226ee4a03d29ee96d13b6889495dbcd
li-li-github <liamiiliil@gmail.com>
2022-03-18 16:35:12 -0700

    added test_gpumemory.py; modified gpumemory.py;

--- cytoself/gpu_utils/gpumemory.py
+++ cytoself/gpu_utils/gpumemory.py
@@ -1,51 +1,90 @@
+import torch
+
+
 def gpuinfo(gpuidx):
     """
     Get GPU information
-    :param gpuidx: GPU index
-    :return: GPU information in dictionary
+
+    Parameters
+    ----------
+    gpuidx : int
+        GPU index
+
+    Returns
+    -------
+    dict :
+        GPU information in dictionary
     """
     import subprocess
 
-    sp = subprocess.Popen(
-        ['nvidia-smi', '-q', '-i', str(gpuidx), '-d', 'MEMORY'],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-    )
-    out_str = sp.communicate()
-    out_list = out_str[0].decode("utf-8").split('BAR1', 1)[0].split('\n')
     out_dict = {}
-    for item in out_list:
-        try:
-            key, val = item.split(':')
-            key, val = key.strip(), val.strip()
-            out_dict[key] = val
-        except Exception as e:
-            print(e)
+    try:
+        sp = subprocess.Popen(
+            ['nvidia-smi', '-q', '-i', str(gpuidx), '-d', 'MEMORY'],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+        out_str = sp.communicate()
+        out_list = out_str[0].decode("utf-8").split('BAR1', 1)[0].split('\n')
+        for item in out_list:
+            try:
+                key, val = item.split(':')
+                key, val = key.strip(), val.strip()
+                out_dict[key] = val
+            except Exception as e:
+                print(e)
+    except Exception as e:
+        print(e)
     return out_dict
 
 
 def getfreegpumem(gpuidx):
     """
     Get free GPU memory
-    :param gpuidx: GPU ID
-    :return: Free memory size
+
+    Parameters
+    ----------
+    gpuidx : int
+        GPU index
+
+    Returns
+    -------
+    int :
+        Free memory size
     """
-    return int(gpuinfo(gpuidx)['Free'].replace('MiB', '').strip())
+    info = gpuinfo(gpuidx)
+    if len(info) > 0:
+        return int(info['Free'].replace('MiB', '').strip())
+    else:
+        return -1
 
 
 def getbestgpu(log_level=0):
     """
-    Get the GPU ID with the largest free memory
-    :param log_level: level to print message; 0: print everything, 1: only print selected device, 2: print nothing
-    :return: the GPU ID with the largest free memory
+    Get the GPU index with the largest free memory
+
+    Parameters
+    ----------
+    log_level : int
+        level to print message; 0: print everything, 1: only print selected device, 2: print nothing
+
+    Returns
+    -------
+    int :
+        The GPU index with the largest free memory
     """
-    freememlist = []
-    for gpuidx in range(4):
-        freemem = getfreegpumem(gpuidx)
-        freememlist.append(freemem)
-        if log_level == 0:
-            print('GPU device %d has %d MiB left.' % (gpuidx, freemem))
-    idbest = freememlist.index(max(freememlist))
-    if log_level < 2:
-        print('--> GPU device %d was chosen' % idbest)
-    return idbest
+    if torch.cuda.is_available():
+        freememlist = []
+        for gpuidx in range(torch.cuda.device_count()):
+            freemem = getfreegpumem(gpuidx)
+            freememlist.append(freemem)
+            if log_level == 0:
+                print('GPU device %d has %d MiB left.' % (gpuidx, freemem))
+        idbest = freememlist.index(max(freememlist))
+        if log_level < 2:
+            print('--> GPU device %d was chosen' % idbest)
+        return idbest
+    else:
+        if log_level < 2:
+            print('No Nvidia GPU was detected.')
+        return -1
---
+++ cytoself/gpu_utils/test/test_gpumemory.py
@@ -0,0 +1,13 @@
+import torch
+from cytoself.gpu_utils.gpumemory import getbestgpu, getfreegpumem, gpuinfo
+
+
+def test_getbestgpu_cpu():
+    if torch.cuda.is_available():
+        assert len(gpuinfo(0)) > 0
+        assert isinstance(getfreegpumem(0), int)
+        assert isinstance(getbestgpu(), int)
+    else:
+        assert len(gpuinfo(0)) == 0
+        assert getfreegpumem(0) == -1
+        assert getbestgpu() == -1
