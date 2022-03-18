1f01f52a11633b86941180a38c87f77b1c4fa400
li-li-github <liamiiliil@gmail.com>
2022-03-18 10:51:04 -0700

    added more tests in test_efficientenc2d.py

--- cytoself/models/encoders/test/test_efficientenc2d.py
+++ cytoself/models/encoders/test/test_efficientenc2d.py
@@ -1,6 +1,5 @@
 from torch import nn
 from torchvision.ops.misc import ConvNormActivation
-from cytoself.models.encoders.efficientenc2d import efficientenc_b0, MBConv
 
 
 block_args = [
@@ -10,6 +9,11 @@ block_args = [
 
 
 def test_efficientenc_b0_default():
+    from cytoself.models.encoders.efficientenc2d import efficientenc_b0
+    model = efficientenc_b0()
+    assert len(model.features._modules) == 7
+    assert isinstance(model.features[0], nn.Sequential)
+    assert isinstance(model.features[-1], nn.Sequential)
     model = efficientenc_b0(block_args)
     assert len(model.features._modules) == len(block_args)
     assert isinstance(model.features[0], nn.Sequential)
@@ -17,9 +21,94 @@ def test_efficientenc_b0_default():
 
 
 def test_efficientenc_b0_channels():
+    from cytoself.models.encoders.efficientenc2d import efficientenc_b0
     model = efficientenc_b0(block_args, in_channels=2, out_channels=64)
     assert len(model.features._modules) == len(block_args) + 2
     assert isinstance(model.features[0], ConvNormActivation)
     assert isinstance(model.features[-1], ConvNormActivation)
     assert model.features[0][0].in_channels == 2
     assert model.features[-1][0].out_channels == 64
+
+
+def test_efficientenc_b1_default():
+    from cytoself.models.encoders.efficientenc2d import efficientenc_b1
+    model = efficientenc_b1()
+    assert len(model.features._modules) == 7
+    assert isinstance(model.features[0], nn.Sequential)
+    assert isinstance(model.features[-1], nn.Sequential)
+    model = efficientenc_b1(block_args)
+    assert len(model.features._modules) == len(block_args)
+    assert isinstance(model.features[0], nn.Sequential)
+    assert isinstance(model.features[-1], nn.Sequential)
+
+
+def test_efficientenc_b2_default():
+    from cytoself.models.encoders.efficientenc2d import efficientenc_b2
+    model = efficientenc_b2()
+    assert len(model.features._modules) == 7
+    assert isinstance(model.features[0], nn.Sequential)
+    assert isinstance(model.features[-1], nn.Sequential)
+    model = efficientenc_b2(block_args)
+    assert len(model.features._modules) == len(block_args)
+    assert isinstance(model.features[0], nn.Sequential)
+    assert isinstance(model.features[-1], nn.Sequential)
+
+
+def test_efficientenc_b3_default():
+    from cytoself.models.encoders.efficientenc2d import efficientenc_b3
+    model = efficientenc_b3()
+    assert len(model.features._modules) == 7
+    assert isinstance(model.features[0], nn.Sequential)
+    assert isinstance(model.features[-1], nn.Sequential)
+    model = efficientenc_b3(block_args)
+    assert len(model.features._modules) == len(block_args)
+    assert isinstance(model.features[0], nn.Sequential)
+    assert isinstance(model.features[-1], nn.Sequential)
+
+
+def test_efficientenc_b4_default():
+    from cytoself.models.encoders.efficientenc2d import efficientenc_b4
+    model = efficientenc_b4()
+    assert len(model.features._modules) == 7
+    assert isinstance(model.features[0], nn.Sequential)
+    assert isinstance(model.features[-1], nn.Sequential)
+    model = efficientenc_b4(block_args)
+    assert len(model.features._modules) == len(block_args)
+    assert isinstance(model.features[0], nn.Sequential)
+    assert isinstance(model.features[-1], nn.Sequential)
+
+
+def test_efficientenc_b5_default():
+    from cytoself.models.encoders.efficientenc2d import efficientenc_b5
+    model = efficientenc_b5()
+    assert len(model.features._modules) == 7
+    assert isinstance(model.features[0], nn.Sequential)
+    assert isinstance(model.features[-1], nn.Sequential)
+    model = efficientenc_b5(block_args)
+    assert len(model.features._modules) == len(block_args)
+    assert isinstance(model.features[0], nn.Sequential)
+    assert isinstance(model.features[-1], nn.Sequential)
+
+
+def test_efficientenc_b6_default():
+    from cytoself.models.encoders.efficientenc2d import efficientenc_b6
+    model = efficientenc_b6()
+    assert len(model.features._modules) == 7
+    assert isinstance(model.features[0], nn.Sequential)
+    assert isinstance(model.features[-1], nn.Sequential)
+    model = efficientenc_b6(block_args)
+    assert len(model.features._modules) == len(block_args)
+    assert isinstance(model.features[0], nn.Sequential)
+    assert isinstance(model.features[-1], nn.Sequential)
+
+
+def test_efficientenc_b7_default():
+    from cytoself.models.encoders.efficientenc2d import efficientenc_b7
+    model = efficientenc_b7()
+    assert len(model.features._modules) == 7
+    assert isinstance(model.features[0], nn.Sequential)
+    assert isinstance(model.features[-1], nn.Sequential)
+    model = efficientenc_b7(block_args)
+    assert len(model.features._modules) == len(block_args)
+    assert isinstance(model.features[0], nn.Sequential)
+    assert isinstance(model.features[-1], nn.Sequential)
